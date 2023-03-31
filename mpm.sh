3e2979db418ee5b31ac7293fe613baa9282c5b4f
Luca Gervasi <luca.gervasi@gmail.com>
2023-03-31 16:02:03 +0200

    Update mpm.sh

--- mpm.sh
+++ mpm.sh
@@ -1,1 +1,20 @@
-ps -ylC httpd --sort:rss | awk '{sum+=$8; ++n} END {print "Tot="sum"("n")";print "Avg="sum"/"n"="sum/n/1024"MB"}'
+ps -ylC httpd --sort:rss | awk '
+{
+  sum+=$8; 
+  ++n
+ } 
+ END {
+  printf("Httpd Tot=%d (%d procs) - Avg=%.2f => %.2f Mb\n", sum, n, sum/n, sum/n/1024);
+ }
+'
+
+
+ps -ylC rotatelogs --sort:rss | awk '
+{
+  sum+=$8; 
+  ++n
+ } 
+ END {
+  printf("Rotatelogs Tot=%d (%d procs) - Avg=%.2f => %.2f Mb\n", sum, n, sum/n, sum/n/1024);
+ }
+'
