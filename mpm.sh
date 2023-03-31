26f3a10e84797bfba61eee5fdf50127304c2010a
Luca Gervasi <luca.gervasi@gmail.com>
2023-03-31 16:06:03 +0200

    Update mpm.sh

--- mpm.sh
+++ mpm.sh
@@ -1,10 +1,16 @@
 ps -ylC httpd --sort:rss | awk '
+BEGIN {
+  min=999999999990;
+  max=0;
+}
 {
-  sum+=$8; 
-  ++n
+  sum+=$8;
+  if ($8 > max) {max=$8;}
+  if ($8 < min) {min=$8;}
+  ++n;
  } 
  END {
-  printf("Httpd      Tot=%d (%d procs) - Avg=%.2f => %.2f Mb\n", sum, n, sum/n, sum/n/1024);
+  printf("Httpd      Tot=%d (%d procs) - Avg=%.2f => %.2f Mb Min=%.2f Max=%.2f\n", sum, n, sum/n, sum/n/1024, min/1024, max/1024);
  }
 '
 
